980814ae708198491b95cd68688ccde25e924e5b
ShadowInSpace <ivan.yackovec@gmail.com>
2022-10-26 23:29:25 +0300

    Second and third finish

--- src/main/java/AllIn.java
+++ src/main/java/AllIn.java
@@ -1,15 +1,16 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class AllIn {
 
     //First Task metod
-    public String filler (ArrayList<String> names){
-        int i =0;
+    public String filler(List<String> names) {
+        int i = 0;
         StringBuilder str = new StringBuilder();
-        for (String name: names) {
-            if(!(i%2==0)){
+        for (String name : names) {
+            if (!(i % 2 == 0)) {
                 str.append(i).append(" ").append(name).append(",");
             }
             i++;
@@ -17,9 +18,25 @@ public class AllIn {
         return str.toString();
     }
 
-//    public ArrayList<String> upperSorter (ArrayList<String> lines){
-//        lines.stream()
-//
-//    }
+    //Second task
+    public List<String> upperSorter(List<String> lines) {
+        return lines.stream()
+                .map(name -> name.toUpperCase())
+                .sorted((aName, bName) -> bName.compareTo(aName))
+                .collect(Collectors.toList());
 
+    }
+
+    //third task
+    public static void NumbersList() {
+        String[] arrey = {"1, 2, 0", "4, 5"};
+
+        String collect = Arrays.stream(arrey)
+                .map(line -> Arrays.asList(line.split(", ")))
+                .flatMap(l -> l.stream())
+                .sorted()
+                .collect(Collectors.joining(", "));
+        System.out.println("collect = " + collect);
+
+    }
 }
